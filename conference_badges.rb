8df1291cc0a9701f736993ff7b5c079d6345b42e
danny galimi <dannygalimi13@aim.com>
2020-08-18 21:31:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,6 +15,5 @@ def assign_rooms(input)
 end
 
 def printer(input)
-puts batch_badge_creator(input)
-puts assign_rooms(input)
+  input.each{puts batch_badge_creator(input), puts assign_rooms(input)}
 end
