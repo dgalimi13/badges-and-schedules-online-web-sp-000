2b555d4a2b38717e49b04e799191a096106162e0
danny galimi <dannygalimi13@aim.com>
2020-08-18 20:40:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,7 +11,7 @@ end
 def assign_rooms(input)
   arr = []
   
-  input.each_with_index{|name, index| arr << "Hello, #{name}! You'll be assigned to room #{index}!"}
+  input.each_with_index{|name, index| arr << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
   
   arr
 end
