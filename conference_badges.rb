84b3a36a6bf65928ea3977e3f6fc063c9bcd427a
danny galimi <dannygalimi13@aim.com>
2020-08-18 21:55:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,15 +2,6 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
- names.map{|name| badge_maker(name)}
-end
 
-def assign_rooms(input)
-  arr = []
-  input.each_with_index{|name, index| arr << "Hello, #{name}! You'll be assigned to room #{index+1}!"}
-  arr
- end
- end
  
 
