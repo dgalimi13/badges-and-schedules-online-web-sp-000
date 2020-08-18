29bc78e5cfacee50b55bd63d97b7ed4bf08352e7
danny galimi <dannygalimi13@aim.com>
2020-08-18 20:20:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,5 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(name)
-  name.each{|message| return "Hello, my name is #{message}"
+  name.each{|message| return "Hello, my name is #{message}}"
 end
